14c39c44f27d3c3abbdbd04e5925c89d84a00899
Kayra Parker-Nance <kayra.parkernance@gmail.com>
2026-07-20 13:45:24 +0200

    Created ParseResult model

---
+++ QuizCreate/QuizCreate.Classes/Models/ParseResult.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizCreate.Classes.Models
+{
+    public class ParseResult
+    {
+        public List<Section> Sections { get; set; } = [];
+        public List<string> Errors { get; set; } = [];
+        public List<string> Warnings { get; set; } = [];
+        public int TotalQuestions => Sections.Sum(s => s.Questions.Count);
+        public bool HasContent => TotalQuestions > 0;
+    }
+}
