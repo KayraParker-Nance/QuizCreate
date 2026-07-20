613ffa5b828192ddefcdc11d601415b7ab031ce9
Kayra Parker-Nance <kayra.parkernance@gmail.com>
2026-07-20 13:47:44 +0200

    Created Theme model

---
+++ QuizCreate/QuizCreate.Classes/Models/Theme.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizCreate.Classes.Models
+{
+    public record Theme(string Name, string Background, string PrimaryColour, string SecondaryColour, string Text, string Muted, string Correct, string Wrong, string Card)
+    {
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
