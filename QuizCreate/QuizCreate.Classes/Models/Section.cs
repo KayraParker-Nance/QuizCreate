bddb640f292283cf7c4882fdcc86112d47621645
Kayra Parker-Nance <kayra.parkernance@gmail.com>
2026-07-20 13:42:20 +0200

    Created Section model

---
+++ QuizCreate/QuizCreate.Classes/Models/Section.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizCreate.Classes.Models
+{
+    public class Section
+    {
+        public string Name { get; set; } = "Questions";
+        public List<Question> Questions { get; set; } = [];
+    }
+}
