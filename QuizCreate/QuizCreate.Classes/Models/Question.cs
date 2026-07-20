312c17274f56b944754314e14b9545567bbda8a7
Kayra Parker-Nance <kayra.parkernance@gmail.com>
2026-07-20 13:40:57 +0200

    Created Question model

---
+++ QuizCreate/QuizCreate.Classes/Models/Question.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizCreate.Classes.Models
+{
+    public class Question
+    {
+        public string Text { get; set; } = "";
+        public List<string> Options { get; set; } = [];
+        public int AnswerIndex { get; set; } = -1;
+        public string Explanation { get; set; } = "";
+    }
+}
